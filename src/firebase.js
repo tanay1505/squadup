fc39ec6f23dbed89be7bec0d68b9cbe286d0c1e7
Tanay <pujaratanay15@gmail.com>
2026-05-26 16:52:43 +0530

    Add env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { initializeAuth, browserLocalPersistence } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQ710DPoSzKBKysz9f67P8oIYUZzt1JU8",
-  authDomain: "squad-up-4fe79.firebaseapp.com",
-  projectId: "squad-up-4fe79",
-  storageBucket: "squad-up-4fe79.firebasestorage.app",
-  messagingSenderId: "491821627474",
-  appId: "1:491821627474:web:100e47a7bdb777d823878b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
-// Use initializeAuth instead of getAuth for more control
-export const auth = initializeAuth(app, {
-  persistence: browserLocalPersistence,
-});
-
-// Must be set IMMEDIATELY after auth is created
-auth.settings.appVerificationDisabledForTesting = true;
+export const auth = getAuth(app);
